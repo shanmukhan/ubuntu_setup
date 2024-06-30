4e12108aa56eb82a85832baad32f03b327897f75
Shanmukhan <shanmukhan.b@gmail.com>
2024-06-30 17:16:28 +0530

    node setup

--- setup.sh
+++ setup.sh
@@ -14,3 +14,17 @@ sudo vi /etc/crontab
 # 3. Install jdk 17
 sudo apt install openjdk-17-jdk
 
+
+# 4. Install Node
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# download and install Node.js (you may need to restart the terminal)
+nvm install 18
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v18.20.3`
+
+# verifies the right NPM version is in the environment
+npm -v # should print `10.7.0`
+
