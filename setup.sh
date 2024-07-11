e9d6374716c1eaea0bde7b14bc92fd5bcf903526
lipi <shanmukhan.b@gmail.com>
2024-07-11 19:39:22 +0530

    adding other routine software installation steps

--- setup.sh
+++ setup.sh
@@ -11,8 +11,9 @@ sudo vi /etc/crontab
 @reboot root echo 80 > /sys/class/power_supply/BAT1/charge_control_end_threshold
 
 
-# 3. Install jdk 17
+# 3. Install jdk 17, 21
 sudo apt install openjdk-17-jdk
+sudo apt install openjdk-21-jdk
 
 
 # 4. Install Node
@@ -28,3 +29,54 @@ node -v # should print `v18.20.3`
 # verifies the right NPM version is in the environment
 npm -v # should print `10.7.0`
 
+
+# 5. Install git
+sudo apt install git
+
+
+# 6. SSH and github setup
+  ssh-keygen -t ed25519 -C "shanmukhan.b@gmail.com"
+  ssh-add ~/.ssh/id_ed25519
+  cat ~/.ssh/id_ed25519.pub
+  pwd
+  ls -l
+  mkdir repo
+  ls -l
+  cd repo/
+  git clone git@github.com:shanmukhan/vantillu.git
+  sudo apt install git
+  git clone git@github.com:shanmukhan/vantillu.git
+  ls -l
+  git clone :shanmukhan/food_pi.git
+  git clone git@github.com:shanmukhan/food_pi.git
+  git clone git@github.com:shanmukhan/vantillu-fe.git
+  git clone git@github.com:shanmukhan/ccme.git
+  git clone git@github.com:shanmukhan/iot.git
+  git clone git@github.com:shanmukhan/ubuntu_setup.git
+
+
+# 7. Install postgres and pgadmin
+  sudo apt install postgresql
+  curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+  sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+  sudo apt install pgadmin4
+
+
+# 8. Install flutter
+  sudo snap install flutter --classic
+
+
+# 9. Install artemis
+  wget https://dlcdn.apache.org/activemq/activemq-artemis/2.35.0/apache-artemis-2.35.0-bin.tar.gz
+  sudo tar -xvzf apache-artemis-2.35.0-bin.tar.gz -C /opt/
+  sudo chown -R artemis: apache-artemis-2.35.0
+  sudo chmod o+x /opt/apache-artemis-2.35.0/bin/
+
+  cd /var/lib
+  sudo /opt/apache-artemis-2.35.0/bin/artemis create ecook
+
+  # you can run the broker in the background using:
+  "/var/lib/ecook/bin/artemis" run
+
+
+# 10.
