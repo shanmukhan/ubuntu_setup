92597c9ab14d6e985ee2dbf2fb5c195daabc08cb
shanmukhan <shanmukhan.b@gmail.com>
2024-07-27 21:56:43 +0530

    adding other routine software installation steps

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 0. Create Group and User
+  sudo addgroup ecook
+  sudo adduser ecook
+  sudo addgroup ecook
+  # sudo useradd -s /bin/false -g ecook -d /opt/artemis ecook
+
+
 # 1. to keep Windows and Ubuntu times are in sync
 timedatectl set-local-rtc 1  # this keeps to save time in Local timezone same as Windows
 
@@ -47,7 +54,7 @@ sudo apt install git
   sudo apt install git
   git clone git@github.com:shanmukhan/vantillu.git
   ls -l
-  git clone :shanmukhan/food_pi.git
+  git clone git@github.com:shanmukhan/food_pi.git
   git clone git@github.com:shanmukhan/food_pi.git
   git clone git@github.com:shanmukhan/vantillu-fe.git
   git clone git@github.com:shanmukhan/ccme.git
@@ -61,12 +68,28 @@ sudo apt install git
   sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
   sudo apt install pgadmin4
 
+  #  lipi@sivakesava:~$ sudo -i -u postgres psql
+  #  postgres=# create user ecook with encrypted password '<>';
+  #  CREATE ROLE
+  #  postgres=# create database vantillu with owner=ecook;
+  #  CREATE DATABASE
+  #  postgres=# ALTER USER postgres with encrypted password '<>';
+  #  ALTER ROLE
+
+  sudo systemctl status postgresql
+  sudo systemctl stop postgresql
+  sudo systemctl start postgresql
+
+  # to connect to psql using ecook user
+  sudo -i -u ecook psql --dbname vantillu
+
+
 
 # 8. Install flutter
   sudo snap install flutter --classic
 
 
-# 9. Install artemis
+# 9. Install artemis  - https://medium.com/@abdullah.eid.2604/setup-activemq-artemis-on-ubuntu-06951244ee8f
   wget https://dlcdn.apache.org/activemq/activemq-artemis/2.35.0/apache-artemis-2.35.0-bin.tar.gz
   sudo tar -xvzf apache-artemis-2.35.0-bin.tar.gz -C /opt/
   sudo chown -R artemis: apache-artemis-2.35.0
@@ -74,6 +97,19 @@ sudo apt install git
 
   cd /var/lib
   sudo /opt/apache-artemis-2.35.0/bin/artemis create ecook
+
+  sudo vi /var/lib/ecook/etc/jolokia-access.xml
+  # and remove localhost to give access to all on ui page
+  # under <cors> - <allowed-origins>
+
+  sudo vi /var/lib/ecook/etc/bootstrap.xml
+  # replace localhost with 0.0.0.0 under <binding> to allow access from every where
+
+  sudo systemctl daemon-reload
+  sudo systemctl enable artemis
+  sudo systemctl start artemis
+
+  # access http://localhost:8161/console/auth/login for admin page
 
   # you can run the broker in the background using:
   "/var/lib/ecook/bin/artemis" run
